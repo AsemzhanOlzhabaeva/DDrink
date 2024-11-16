856c613aabd6514bf4f9af66bb8897ce377cacf2
MacBook <macbook@Air-MacBook.Dlink>
2024-11-17 00:40:52 +0500

    Initial commit

---
+++ src/main/java/kzittu/DDrinkApplication.java
@@ -0,0 +1,13 @@
+package kzittu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDrinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DDrinkApplication.class, args);
+	}
+
+}
