2d54712a37b092b518d3717f5fc540a1568f7c35
macbook <johnlennonono0@gamil.com>
2024-11-19 12:01:57 +0500

    init

--- src/main/java/Model/Beverage.java
+++ src/main/java/Model/Beverage.java
@@ -1,4 +1,18 @@
 package Model;
 
-public class Beverage {
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class Beverage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    private double volume; // Объем в литрах
 }
